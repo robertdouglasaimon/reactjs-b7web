db5ce15ade62a28c937e534f47d06f00c094118c
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-07-10 13:22:15 -0300

    UPDATE

---
+++ projetos/aula pratica/b7webprojeto-em-nextjs/src/app/page.js
@@ -0,0 +1,12 @@
+
+const Texto = () => {
+    return (
+        <div>
+            <h1>Olá mundo, denovo! kkkk</h1>
+            <h2>Testando essa bagaça!</h2>
+            <h3> Acho que deu certo :D</h3>
+        </div>
+    );
+};
+
+export default Texto;
